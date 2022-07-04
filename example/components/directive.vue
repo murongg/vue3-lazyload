0cfa87d2a715176a25b92a2a5b2f494451995734
木荣 <372638156@qq.com>
2022-07-04 10:45:56 +0800

    chore: demo image url update

--- example/components/directive.vue
+++ example/components/directive.vue
@@ -2,21 +2,10 @@
   <div class="margin" />
   <!-- <img v-lazy="'/example/assets/logo.png'" alt="Vue logo" width="100"> -->
   <h1>Primary</h1>
-  <img
-    v-lazy="{ src: errorlazy.src, lifecycle: errorlazy.lifecycle }"
-    alt="Vue logo"
-    class="image"
-    width="100"
-  />
+  <img v-lazy="{ src: errorlazy.src, lifecycle: errorlazy.lifecycle }" alt="error image" class="image" width="100" />
   <button @click="change">change</button>
   <h1>v-for</h1>
-  <img
-    v-for="item in defaultImages"
-    v-lazy="{ src: item }"
-    alt="Vue logo"
-    class="image"
-    width="100"
-  />
+  <img v-for="item in defaultImages" v-lazy="{ src: item }" alt="Vue logo" class="image" width="100" />
 </template>
 
 <script>
@@ -39,17 +28,16 @@ export default {
       }
     })
     const change = () => {
-      errorlazy.src = 'http://img.mm4000.com/file/8/91/3b3c5819be_1044.jpg'
+      errorlazy.src = 'https://picsum.photos/300/300'
     }
     return {
       errorlazy,
       change,
       defaultImages: [
-        'http://img.mm4000.com/file/8/91/3b3c5819be_1044.jpg',
-        'http://img.mm4000.com/file/8/91/ec3ee1aeed_1044.jpg',
-        'http://img.mm4000.com/file/8/91/b177ba87cf_1044.jpg',
-        'http://img.mm4000.com/file/8/91/8a3bb1e8ba_1044.jpg',
-        'http://img.mm4000.com/file/8/91/77f37a191f_1044.jpg'
+        'https://picsum.photos/200/300',
+        'https://picsum.photos/300/300',
+        'https://picsum.photos/400/300',
+        'https://picsum.photos/500/300',
       ]
     }
   }
@@ -60,15 +48,19 @@ export default {
 .margin {
   margin-top: 1000px;
 }
+
 .image {
   display: block;
 }
+
 .image[lazy="loading"] {
   background: goldenrod;
 }
+
 .image[lazy="error"] {
   background: red;
 }
+
 .image[lazy="loaded"] {
   background: green;
 }
